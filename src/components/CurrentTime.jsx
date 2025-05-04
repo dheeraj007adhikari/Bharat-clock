818d2d98a2583f89fe35364909874b37f66a1708
Dheeraj Adhikri <adhkaridheeraj007@gmail.com>
2025-05-04 16:41:30 +0530

    adding useEffect

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 let CurrentTime = () => {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("interval has been setup");
+    const intervalId = setInterval(() => {
+      let newTime = new Date();
+      setTime(newTime);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+      console.log("cancel the interval");
+    };
+  }, []);
 
   return (
     <p className="lead">
